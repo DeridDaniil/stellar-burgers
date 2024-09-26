e3a30d28343917e7a9c42a09c4c1d36f6636a1f7
Derid Daniil <derid.daniil@yandex.ru>
2024-09-26 17:48:02 +0500

    first review

--- src/pages/constructor-page/constructor-page.tsx
+++ src/pages/constructor-page/constructor-page.tsx
@@ -1,19 +1,27 @@
-import { useSelector } from '../../services/store';
+import { useDispatch, useSelector } from '../../services/store';
 
 import styles from './constructor-page.module.css';
 
 import { BurgerIngredients } from '../../components';
 import { BurgerConstructor } from '../../components';
 import { Preloader } from '../../components/ui';
-import { FC } from 'react';
+import { FC, useEffect } from 'react';
+import {
+  getIngredientsList,
+  isLoadingState
+} from '../../services/slices/ingredients';
 
 export const ConstructorPage: FC = () => {
-  /** TODO: взять переменную из стора */
-  const isIngredientsLoading = false;
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getIngredientsList());
+  }, []);
+
+  const isLoading = useSelector(isLoadingState);
 
   return (
     <>
-      {isIngredientsLoading ? (
+      {isLoading ? (
         <Preloader />
       ) : (
         <main className={styles.containerMain}>
